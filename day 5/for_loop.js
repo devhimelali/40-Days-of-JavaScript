067d6c8a2d4b39fd10d67aba49752f3cb5095e4e
devhimelali <dev.himelali@gmail.com>
2025-04-21 09:53:17 +0600

    add script to find sum of even numbers from 1 to 100 using for loop

--- day 5/for_loop.js
+++ day 5/for_loop.js
@@ -2,3 +2,14 @@
 for(let i =1 ; i <= 10; i++){
     console.log('Iteration is: ' + i);
 }
+
+
+// write a program to find sum of even numbers from 1 to 100 using for loop
+let sum = 0;
+for(let i = 1; i <= 100; i++){
+    if(i % 2 == 0){
+        sum += i;
+    }
+}
+
+console.log('Sum of even numbers is: ' + sum);
